b3cfb2ddaf097c3f5b595d89f8abca3b9f514af0
north-tower <muneshmyke@gmail.com>
2024-07-07 14:23:33 +0300

    Homescreen Image

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,24 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
+import tw from 'twrnc'
+import { Ionicons } from '@expo/vector-icons'
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={tw`flex-1 bg-gray-100 relative`}> 
+    <ScrollView>
+        <TouchableOpacity style={tw`absolute z-50 top-5 right-10 items-center`}>
+            <Ionicons name="person-circle" size={24} color="black" />
+            <Text>User</Text>
+        </TouchableOpacity>
+     
+      <Image 
+        source={{ uri: "https://i.postimg.cc/zB2Hs1SP/e14NE49.png" }} 
+        style={tw`w-full h-64`}
+        onLoad={() => console.log('Image loaded')} 
+        onError={() => console.log('Image failed to load')} 
+        />
+</ScrollView>
     </SafeAreaView>
   )
 }
