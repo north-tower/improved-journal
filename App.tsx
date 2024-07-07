6932ac26eaa57f120e8cde084a856476aa58b7b9
north-tower <muneshmyke@gmail.com>
2024-07-07 13:49:26 +0300

    reinitialized

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <SafeAreaView style={tw`flex-1 items-center justify-center bg-red-500`}> 
+          <Text style={tw`text-white`}>Open up App.tsx to start working on your app!</Text>
+        </SafeAreaView>
+      </Stack.Navigator>
+    </NavigationContainer>
+   
   );
 }
 
